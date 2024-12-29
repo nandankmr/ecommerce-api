756e99ab2c62d07d4e806922cbd97da61d1f1662
nandankmr <nandankmrjha@gmail.com>
2024-12-29 22:53:15 +0530

    chore: removed unnecessary code

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,6 @@
 export const DATABASE_URL = process.env.DATABASE_URL;
 
 export const PORT = process.env.PORT ?? 3000;
-export const APP_BASE_URL = process.env.APP_BASE_URL ?? '';
 
 // CORS IP / Domain White listed list
 if (typeof process.env.CORS_WHITELIST !== 'string') {
@@ -10,16 +9,7 @@ if (typeof process.env.CORS_WHITELIST !== 'string') {
 }
 export const CORS_WHITELIST = process.env.CORS_WHITELIST ?? '';
 
-// API Rate Limiting config
-if (
-  typeof process.env.RATE_LIMIT_TIME !== 'string' ||
-  typeof process.env.RATE_LIMIT_REQ_COUNT !== 'string'
-) {
-  console.warn(
-    'Request rate limit configuration is missing!'
-  );
-}
 export const RATE_LIMIT = {
-  TIME: process.env.RATE_LIMIT_TIME,
-  COUNT: process.env.RATE_LIMIT_REQ_COUNT,
+  TIME: 1,
+  COUNT: 100,
 };
