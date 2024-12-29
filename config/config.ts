44bf39f41d0952dbcb6b7f0dbd0c8147ae1d5b39
nandankmr <nandankmrjha@gmail.com>
2024-12-30 00:33:30 +0530

    chore: remove vercel config

--- config/config.ts
+++ config/config.ts
@@ -7,7 +7,7 @@ export const PORT = process.env.PORT ?? 3000;
 if (typeof process.env.CORS_WHITELIST !== 'string') {
   console.warn('CORS whitelist is missing!');
 }
-export const CORS_WHITELIST = process.env.CORS_WHITELIST ?? '';
+export const CORS_WHITELIST = process.env.CORS_WHITELIST ?? '*';
 
 export const RATE_LIMIT = {
   TIME: 1,
